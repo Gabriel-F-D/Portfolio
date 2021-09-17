ce86b85ad33d34ea22e27cd0fb51669e1efa3a55
Gabriel-F-D <gdelgadillo299@gmail.com>
2021-09-17 20:35:52 -0300

    added projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,10 +5,7 @@ import Particle from "../Particle";
 
 import doggo from "../../Assets/Projects/doggo.png";
 import MdPrev from "../../Assets/Projects/MdPrev.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import weather from "../../Assets/Projects/weather.png";
 
 function Projects() {
   return (
@@ -34,7 +31,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={weather}
               isBlog={false}
               title="Weather app"
               description="Website build with React and tailwind, it makes an api request of the current weather each time the user types a city in the input."
@@ -42,35 +39,7 @@ function Projects() {
             />
           </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={editor}
-              isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              link="https://github.com/soumyajit4419/Editor.io"
-            />
-          </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={bitsOfCode}
-              isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              link="https://github.com/soumyajit4419/Bits-0f-C0de"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              link="https://github.com/soumyajit4419/AI_For_Social_Good"
-            />
-          </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
